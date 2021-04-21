042f7ad5a2e77ec589e839473ef4db48b8000f6a
lirsacc <c.lirsac@gmail.com>
2021-04-21 17:58:39 +0100

    Django 2.2

---
+++ sqlmigrate.sql
@@ -0,0 +1,14 @@
+BEGIN;
+--
+-- Create model Price
+--
+CREATE TABLE "prices_price" ("id" serial NOT NULL PRIMARY KEY, "price" integer NOT NULL, "price_previous" integer NOT NULL, "on_sale" boolean NOT NULL);
+--
+-- Create constraint on_sale_check on model price
+--
+ALTER TABLE "prices_price" ADD CONSTRAINT "on_sale_check" CHECK ("on_sale" = ("prices_price"."price" < ("prices_price"."price_previous")));
+--
+-- Create constraint price_lte_price_previous on model price
+--
+ALTER TABLE "prices_price" ADD CONSTRAINT "price_lte_price_previous" CHECK ("price" <= ("price_previous"));
+COMMIT;
