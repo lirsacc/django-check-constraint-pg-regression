938f1ba93562a1d1f3ed5bb5d7d26e0aa7bba1cf
lirsacc <c.lirsac@gmail.com>
2021-04-21 17:42:57 +0100

    Common PG setup

--- regression_test/settings.py
+++ regression_test/settings.py
@@ -78,10 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'regression_test',
-        'USER': 'charles',
+        'USER': '',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '5832',
+        'PORT': '5432',
     }
 }
 
